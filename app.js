4dd62f5b6764624a492432f3862ad2f43e781dae
Anthony <astrowelding@gmail.com>
2022-07-11 10:10:30 -0700

    upload test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.set('views', path.join(_dirname,'views'));
+// app.set('views', path.join(_dirname,'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
