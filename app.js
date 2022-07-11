8b82f39ff410cdc7627ce0afaad2fa04bf4587ea
Anthony <astrowelding@gmail.com>
2022-07-11 09:58:13 -0700

    pre pre live

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-app.use(express.static("public"));
+const port = process.env.PORT || 3000
+
+app.set('views', path.join(_dirname,'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
 
 app.get("/" , function(req,res){
     res.render("index")
 })
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("server started on port 3000");
 })
